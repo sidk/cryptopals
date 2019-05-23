68773620da3bd12030f400583dac8152ef922482
Sid Krishnan <krishnan.sid@gmail.com>
2019-05-23 15:51:05 -0400

    clean up padding oracle code

--- src/cbcPaddingOracle.js
+++ src/cbcPaddingOracle.js
@@ -20,12 +20,9 @@ const iv = Buffer.alloc(16, 0);
 
 export const encrypt = (): Buffer => {
   const stringToEncrypt = Buffer.from(
-    // "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
-    // "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
     stringsToEncrypt[Math.floor(Math.random() * 10)],
     "base64"
   );
-  // const stringToEncrypt = Buffer.from("0000000000000000 RlowerUPPERl");
   console.log("Encrypting String: ", stringToEncrypt.toString("ascii"));
   return encryptCBC(pad(stringToEncrypt, 16), key, iv);
 };
@@ -47,49 +44,15 @@ export const paddingOracleAttack = (
       const cPrevAtIndex = Array.from({ length: 256 }, (_, i) => i).find(
         byte => {
           const attackBuffer = Buffer.concat([
-            Buffer.alloc(15 - i, 0),
+            Buffer.alloc(15 - i, 0), // this is key. instead of using cPrev, ensure that all bytes not of interest act as passthroughs
             Buffer.from([byte]),
             xor(Buffer.from(dk), Buffer.alloc(i, i + 1)),
             cCurrent
           ]);
 
           try {
-            const valid = isPaddingValid(attackBuffer);
-            // we have valid padding, but we can't be sure that the padding is what we think it is
-            // when i === 0, it has to be a padding of 0x01.
-            // so we do some more checking below on the last byte (i === 0):
-            if (i === 0 && byte !== cPrev[15]) {
-              // Our attack buffer is [0, 0, 0, ..., byte]
-              // 1. It is possible that the existing xor(cPrev[last byte], dk[ last byte ]) is already 0x01.
-              //    In this case, we want to use cPrev[last byte] to calculate dk[last byte]
-              try {
-                const existingCPrevValid = isPaddingValid(
-                  Buffer.concat([
-                    Buffer.alloc(15, 0),
-                    Buffer.from([cPrev[15]]),
-                    cCurrent
-                  ])
-                );
-                if (existingCPrevValid) {
-                  return false;
-                }
-              } catch (e) {
-                try {
-                  isPaddingValid(
-                    Buffer.concat([
-                      Buffer.alloc(14, 0),
-                      Buffer.from([255, byte]),
-                      cCurrent
-                    ])
-                  );
-                  return valid;
-                } catch (e) {
-                  return false;
-                }
-              }
-            } else {
-              return true;
-            }
+            isPaddingValid(attackBuffer);
+            return true;
           } catch (e) {
             return false;
           }
