63fbaf83782ea42f3530dc7c35ba82a384287935
Sid Krishnan <krishnan.sid@gmail.com>
2019-06-23 18:57:48 -0400

    implement MT19937 cloning attack

--- src/MT19937.js
+++ src/MT19937.js
@@ -1,55 +1,158 @@
-const f = BigInt(1812433253); // constant used for initialization/seeding
-const w = BigInt(32); // word-size in bits
-const n = BigInt(624); // degree of recurrence - i think this refers to how many steps to look back to arrive at the next value in the series
-const max = BigInt(4294967295);
-const upperMask = BigInt(parseInt("80000000", 16)); // (1 << r) - 1; // this is 31(r) bits of 1
-const lowerMask = BigInt(parseInt("7fffffff", 16)); // ~lowerMask; // lowest w bits of (not lower_mask)
-const a = BigInt(parseInt("9908B0DF", 16));
-const m = BigInt(397);
-const u = BigInt(11);
-const d = BigInt(parseInt("FFFFFFFF", 16));
-const s = BigInt(7);
-const b = BigInt(parseInt("9D2C5680", 16));
-const t = BigInt(15);
-const c = BigInt(parseInt("EFC60000", 16));
-const l = BigInt(18);
+import bigInt from "big-integer";
 
-const state = []; // state is an array of n values of w bits each
-let index = 0;
-
-export const seed = seed => {
-  // seed is a w-bit/32-bit value
-  state.push(BigInt(seed));
-  [...Array(Number(n) - 1).keys()].forEach(i => {
-    // MT[i] := lowest w bits of (f * (MT[i-1] xor (MT[i-1] >> (w-2))) + i)
-    const shiftedState = state[i] >> (w - BigInt(2));
-    const fullState = f * (state[i] ^ shiftedState) + BigInt(i + 1);
-    state.push(BigInt(fullState & max));
-  });
-  return state;
+export const CONSTANTS = {
+  a: bigInt(parseInt("9908B0DF", 16)),
+  b: bigInt(parseInt("9D2C5680", 16)),
+  c: bigInt(parseInt("EFC60000", 16)),
+  d: bigInt(parseInt("FFFFFFFF", 16)),
+  f: bigInt(1812433253), // constant used for initialization/seeding
+  l: bigInt(18),
+  lowerMask: bigInt(parseInt("7fffffff", 16)), // ~lowerMask, // lowest w bits of (not lower_mask)
+  m: bigInt(397),
+  max: bigInt(4294967295),
+  n: bigInt(624), // degree of recurrence
+  s: bigInt(7),
+  t: bigInt(15),
+  u: bigInt(11),
+  upperMask: bigInt(parseInt("80000000", 16)), // (1 << r) - 1, // this is 31(r) bits of 1
+  w: bigInt(32) // word-size in bits
 };
 
-export const twist = () => {
-  [...Array(Number(n)).keys()].forEach(i => {
-    const x = (state[i] & upperMask) + (state[BigInt(i + 1) % n] & lowerMask);
-    let xA = x >> BigInt(1);
-    if (x % BigInt(2) !== BigInt(0)) {
-      xA = xA ^ a;
-    }
-    state[i] = state[(BigInt(i) + m) % n] ^ xA;
-  });
-};
+// let state; // state is an array of n values of w bits each
+// let index;
 
-export const extract = () => {
-  if (index === 0) {
-    twist();
+export class MT19937 {
+  constructor(seed) {
+    // seed is a w-bit/32-bit value
+    this.state = [];
+    this.index = 0;
+    this.state.push(bigInt(seed));
+    [...Array(Number(CONSTANTS.n) - 1).keys()].forEach(i => {
+      // MT[i] := lowest w bits of (f * (MT[i-1] xor (MT[i-1] >> (w-2))) + i)
+      const shiftedState = this.state[i].shiftRight(
+        CONSTANTS.w.minus(bigInt(2))
+      );
+      const fullState = CONSTANTS.f
+        .times(this.state[i].xor(shiftedState))
+        .plus(bigInt(i + 1));
+      this.state.push(fullState.and(CONSTANTS.max));
+    });
   }
-  let y;
-  y = state[index];
-  y = y ^ ((y >> u) & d);
-  y = y ^ ((y << s) & b);
-  y = y ^ ((y << t) & c);
-  y = y ^ (y >> l);
-  index = (index + 1) % Number(n);
-  return y & max;
+
+  twist() {
+    [...Array(Number(CONSTANTS.n)).keys()].forEach(i => {
+      const x = this.state[i]
+        .and(CONSTANTS.upperMask)
+        .plus(
+          this.state[bigInt(i + 1).mod(CONSTANTS.n)].and(CONSTANTS.lowerMask)
+        );
+      let xA = x.shiftRight(bigInt(1));
+      if (x.mod(bigInt(2)).compare(bigInt(0))) {
+        xA = xA.xor(CONSTANTS.a);
+      }
+      this.state[i] = this.state[
+        bigInt(i)
+          .plus(CONSTANTS.m)
+          .mod(CONSTANTS.n)
+      ].xor(xA);
+    });
+  }
+
+  extract() {
+    if (this.index === 0) {
+      this.twist();
+    }
+    let y;
+    y = this.state[this.index];
+    y = y.xor(y.shiftRight(CONSTANTS.u).and(CONSTANTS.d));
+    y = y.xor(y.shiftLeft(CONSTANTS.s).and(CONSTANTS.b));
+    y = y.xor(y.shiftLeft(CONSTANTS.t).and(CONSTANTS.c));
+    y = y.xor(y.shiftRight(CONSTANTS.l));
+    this.index = (this.index + 1) % Number(CONSTANTS.n);
+    return y.and(CONSTANTS.max);
+  }
+
+  setState(state) {
+    this.state = state;
+  }
+}
+
+const ones = bitWidth =>
+  bigInt(1)
+    .shiftLeft(bitWidth)
+    .minus(1);
+
+const zeroOutRight = (val, bitWidth) =>
+  val.shiftRight(bitWidth).shiftLeft(bitWidth);
+
+const zeroOutLeft = (val, bitWidth) =>
+  val
+    .shiftLeft(bitWidth)
+    .and(ones(bigInt(32)))
+    .shiftRight(bitWidth);
+
+export const reverseRightShiftXor = (y, constant) => {
+  const bitWidth = bigInt(y.toString(2).length);
+  const sTop = zeroOutRight(y, bitWidth - constant);
+  const yTop = y.shiftRight(constant);
+  let sBottom = y.and(ones(bitWidth - constant)).xor(yTop);
+  return sTop.or(sBottom);
+};
+
+const ONE = bigInt(1);
+const TWO = bigInt(2);
+const BITWIDTH = bigInt(32);
+
+export const reverseRightShiftXorAnd = (y, shiftConstant, andConstant) => {
+  // andConstant is always 32 bits, which is the maximum bit width
+  const sTop = zeroOutRight(y, BITWIDTH - shiftConstant);
+  const numChunks = Math.floor(Number(BITWIDTH / shiftConstant)); // we do floor here because the first chunk is sTop
+  return [...Array(numChunks).keys()].reduce((sBottom, i) => {
+    const I = bigInt(i);
+    return sBottom.or(
+      andConstant
+        .and(
+          ones(BITWIDTH.minus(I.plus(ONE).times(shiftConstant))).and(
+            sBottom.shiftRight(shiftConstant)
+          )
+        )
+        .xor(
+          zeroOutRight(
+            y.and(ones(BITWIDTH.minus(I.plus(ONE).times(shiftConstant)))),
+            bigInt.max(0, BITWIDTH.minus(I.plus(TWO).times(shiftConstant)))
+          )
+        )
+    );
+  }, sTop);
+};
+
+export const reverseLeftShiftXorAnd = (y, shiftConstant, andConstant) => {
+  const sBottom = zeroOutLeft(y, BITWIDTH - shiftConstant);
+  const numChunks = Math.floor(Number(BITWIDTH / shiftConstant)); // we do floor here because the first chunk is sBottom
+  return [...Array(numChunks).keys()].reduce((sTop, i) => {
+    const I = bigInt(i);
+    return sTop.or(
+      andConstant
+        .and(
+          ones(BITWIDTH)
+            .shiftLeft(I.plus(ONE).times(shiftConstant))
+            .and(sTop.shiftLeft(shiftConstant))
+        )
+        .xor(
+          zeroOutLeft(
+            y.and(ones(BITWIDTH).shiftLeft(I.plus(ONE).times(shiftConstant))),
+            bigInt.max(0, BITWIDTH.minus(I.plus(TWO).times(shiftConstant)))
+          )
+        )
+    );
+  }, sBottom);
+};
+
+export const untemper = y => {
+  let s;
+  s = reverseRightShiftXor(y, CONSTANTS.l);
+  s = reverseLeftShiftXorAnd(s, CONSTANTS.t, CONSTANTS.c);
+  s = reverseLeftShiftXorAnd(s, CONSTANTS.s, CONSTANTS.b);
+  s = reverseRightShiftXorAnd(s, CONSTANTS.u, CONSTANTS.d);
+  return s;
 };
--- src/MT19937.test.js
+++ src/MT19937.test.js
@@ -1,6 +1,52 @@
-import { seed, extract } from "./MT19937";
+import {
+  CONSTANTS,
+  reverseRightShiftXor,
+  reverseRightShiftXorAnd,
+  reverseLeftShiftXorAnd,
+  untemper,
+  MT19937
+} from "./MT19937";
+import _ from "lodash";
+import bigInt from "big-integer";
 
-test("rand", () => {
-  seed(5489);
-  console.log(extract(), extract(), extract());
+test("crack RNG seed", () => {
+  let secretSeed;
+  const generateRN = () => {
+    const s = new Date().getTime() + Math.floor(Math.random() * 1000);
+    console.log("we generated rn with seed:", s);
+    secretSeed = s;
+    const rng = new MT19937(secretSeed);
+    return rng.extract();
+  };
+
+  const rn = generateRN();
+  const currentTime = new Date().getTime();
+  const theSeed = _.range(currentTime - 20000, currentTime + 20000).find(s => {
+    const rng = new MT19937(s);
+    return rng.extract().compare(rn) === 0;
+  });
+  expect(theSeed).toEqual(secretSeed);
+});
+
+test("clone MT19937", () => {
+  const rng = new MT19937(12345);
+  const nTimes = [...Array(Number(624)).keys()];
+  const nRandomNums = nTimes.map(n => rng.extract());
+  expect(reverseRightShiftXor(bigInt(3992670690), CONSTANTS.l)).toEqual(
+    bigInt(3992670690).xor(bigInt(3992670690).shiftRight(bigInt(18)))
+  );
+  expect(
+    reverseRightShiftXorAnd(bigInt(1421421724), CONSTANTS.u, CONSTANTS.d)
+  ).toEqual(bigInt(1421064939));
+
+  const s = bigInt(12345678);
+  const y = s.xor(s.shiftLeft(CONSTANTS.s).and(CONSTANTS.b));
+  expect(reverseLeftShiftXorAnd(y, CONSTANTS.s, CONSTANTS.b)).toEqual(s);
+
+  const state = nRandomNums.map(untemper);
+  const rng2 = new MT19937(0);
+  rng2.setState(state);
+  expect(nTimes.map(n => rng2.extract())).toEqual(
+    nTimes.map(n => rng.extract())
+  );
 });
